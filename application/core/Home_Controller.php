f4137881ebdf013def125f1e4af0ddffcadb2f37
luanmattos <patrickluan.matos@gmail.com>
2020-12-09 13:35:26 -0300

    save

--- application/core/Home_Controller.php
+++ application/core/Home_Controller.php
@@ -15,7 +15,6 @@ class Home_Controller extends SI_Controller {
     }
 
     private function authRequest(){
-        var_dump($_SERVER);
 
         if( ENVIRONMENT == 'production' ){
 
@@ -38,7 +37,7 @@ class Home_Controller extends SI_Controller {
     private function _headers(){
         header('Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT');
         header('Access-Control-Allow-Origin: *');
-        header('Access-Control-Allow-Headers: Origin, Authorization, Client-Security-Token, Accept-Encoding, X-Auth-Token, X-Requested-With, Content-Type, Accept, x-Access-Token');
+        header('Access-Control-Allow-Headers:  Authorization, Client-Security-Token, Accept-Encoding, X-Auth-Token, X-Requested-With, Content-Type, Accept, x-Access-Token');
         header('Content-type: application/json');
     }
 
