adb942464a4fabca690725850d5ad718268083e4
luanmattos <patrickluan.matos@gmail.com>
2020-12-11 17:59:54 -0300

    save

--- application/core/Home_Controller.php
+++ application/core/Home_Controller.php
@@ -12,7 +12,7 @@ class Home_Controller extends SI_Controller {
         parent::__construct();
         $this->setConfigs();
         $this->authRequest();
-        echo "cron funcionou";
+        echo "cron funcionou porraaaa";
     }
 
     private function authRequest(){
