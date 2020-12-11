a6fb9cedb807f1f265b78108b2b1385b902597c0
luanmattos <patrickluan.matos@gmail.com>
2020-12-11 18:56:38 -0300

    cron hour

--- application/core/Home_Controller.php
+++ application/core/Home_Controller.php
@@ -12,7 +12,7 @@ class Home_Controller extends SI_Controller {
         parent::__construct();
         $this->setConfigs();
         $this->authRequest();
-        echo "AGROA VAI";
+        echo date('d/m/Y H:i:s');
     }
 
     private function authRequest(){
