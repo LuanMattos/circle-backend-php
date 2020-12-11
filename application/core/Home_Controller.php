c64a7a28277e4dd568fb2b9e7e6396b47ba47cd4
luanmattos <patrickluan.matos@gmail.com>
2020-12-11 18:43:13 -0300

    deploy gato

--- application/core/Home_Controller.php
+++ application/core/Home_Controller.php
@@ -12,7 +12,7 @@ class Home_Controller extends SI_Controller {
         parent::__construct();
         $this->setConfigs();
         $this->authRequest();
-        echo "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+        echo "Cron bombo meu velho";
     }
 
     private function authRequest(){
