9475323531906f8801e6d1e1796e94692278a27b
luanmattos <patrickluan.matos@gmail.com>
2020-12-11 22:58:57 -0300

    cors adptado pela milésima vez

--- application/core/Home_Controller.php
+++ application/core/Home_Controller.php
@@ -15,7 +15,7 @@ class Home_Controller extends SI_Controller {
     }
 
     private function authRequest(){
-        echo $_SERVER['HTTP_ORIGIN'];
+
 //        if( ENVIRONMENT === 'production' ){
 //
 //            if( $_SERVER['HTTP_ORIGIN'] === $this->prod) {
