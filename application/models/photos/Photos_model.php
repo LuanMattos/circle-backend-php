42800aeff1ede32fa0d98ce9b520ca44d84cd98e
luanmattos <patrickluan.matos@gmail.com>
2020-12-31 12:59:00 -0300

    Resolvendo problema imagem photo pagina do usuario

--- application/models/photos/Photos_model.php
+++ application/models/photos/Photos_model.php
@@ -13,13 +13,36 @@ class Photos_model extends CI_Model{
 
     public function getPhotoUser( $userId,$dataUserLogged, $orderby ,$direction, $limit = 9, $offset ){
 
-        $photos = $this->getWhere([ 'user_id' => $userId ],"array", $orderby, $direction, $limit, $offset );
+        $fields = [
+            'p.photo_id',
+            'p.photo_post_date',
+            'p.photo_url',
+            'p.photo_description',
+            'p.photo_allow_comments',
+            'p.photo_likes',
+            'p.photo_comments',
+            'p.photo_public',
+            'u.user_name',
+            'u.user_full_name',
+            'u.user_avatar_url',
+            'u.user_cover_url'
+        ];
+
+        $photos = $this->db
+            ->select( $fields )
+            ->from("photo p")
+            ->join("user u", "u.user_id = p.user_id","join")
+            ->order_by($orderby,$direction)
+            ->where("u.user_id", $userId)
+            ->limit($limit)
+            ->offset( $offset )
+            ->get()
+            ->result_array();
 
         if( $dataUserLogged ){
             foreach ( $photos as $key=>$item ) {
                 $photos[$key]['likes'] = [];
                 $photos[$key]['liked'] = $this->Likes_model->likedMe($item['photo_id'],$dataUserLogged->user_id,"row")?true:false;
-                $photos[$key]['user'] = $this->User_model->dataUserPhoto( $userId );
             }
         }
         return $photos;
