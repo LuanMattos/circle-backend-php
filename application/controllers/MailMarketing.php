4268e7f3277c60bc2b4e52bee680af6e8fb390d4
luanmattos <patrickluan.matos@gmail.com>
2021-01-11 00:32:10 -0300

    save mail correction

--- application/controllers/MailMarketing.php
+++ application/controllers/MailMarketing.php
@@ -1,5 +1,5 @@
 <?php
-//use Services\Domain\User\EmailService;
+use Services\Domain\User\EmailService;
 class MailMarketing extends Home_Controller
 {
     private $emailService;
@@ -7,14 +7,13 @@ class MailMarketing extends Home_Controller
     public function __construct(){
         parent::__construct();
         $this->load->model('mail/Email_marketing_model');
-        $this->load->library('email/mail');
-//        $this->emailService = new EmailService\EmailService();
+        $this->emailService = new EmailService\EmailService();
     }
 
     public function index(){
-//        $email = $this->Email_marketing_model->getWhere(['email_marketing_sent'=>'f'], "row");
-debug('dfs');
-        $email = false;
+        $email = $this->Email_marketing_model->getWhere(['email_marketing_sent'=>'f'], "row");
+
+        $email = 'patrick.mattos@compasso.com.br';
         if( $email ){
             $this->sendMail( $email );
         }
@@ -24,22 +23,6 @@ debug('dfs');
     }
     public function sendMail( $email ){
 //        $this->Email_marketing_model->save(['email_marketing_id'=>$email->email_marketing_id,'email_marketing_sent'=>'t']);
-        $this->sendEmail( $email );
-    }
-    public function sendEmail( $emailTo ){
-        $emailFromMarketing = $this->config->item('email_account_marketing');
-
-
-        $mail  = new \Mail();
-        $param = [];
-        $param['from']              = $emailFromMarketing;
-        $param['to']                = $emailTo;
-        $param['name']              = "Circle";
-
-        $html = $this->load->view("email/marketing", NULL,true);
-        $param['corpo']      = '';
-        $param['corpo_html'] = $html;
-        $send = $mail->send( $param );
-        debug($send);
+        $this->emailService->sendEmail( $email );
     }
 }
