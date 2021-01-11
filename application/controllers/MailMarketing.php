906459d3a9236318ba4b95f2ecbd6e0f6b66f968
luanmattos <patrickluan.matos@gmail.com>
2021-01-11 00:30:38 -0300

    cron email

--- application/controllers/MailMarketing.php
+++ application/controllers/MailMarketing.php
@@ -14,7 +14,7 @@ class MailMarketing extends Home_Controller
     public function index(){
 //        $email = $this->Email_marketing_model->getWhere(['email_marketing_sent'=>'f'], "row");
 debug('dfs');
-        $email = 'patrick.mattos@compasso.com.br';
+        $email = false;
         if( $email ){
             $this->sendMail( $email );
         }
