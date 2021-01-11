7e942b18f0e52d333f0fa366f5be501de8570578
luanmattos <patrickluan.matos@gmail.com>
2021-01-11 00:17:07 -0300

    cron email

--- index.php
+++ index.php
@@ -54,11 +54,11 @@
  * NOTE: If you change these, also change the error_reporting() code below
  */
 //define('ENVIRONMENT', $_SERVER['HTTP_HOST'] === 'localhost'  ? 'development' : 'production');
-//if(strstr($_SERVER['HTTP_HOST'],"localhost")){
+if(strstr($_SERVER['HTTP_HOST'],"localhost")){
     define('ENVIRONMENT','development' );
-//}else{
-//    define('ENVIRONMENT','production' );
-//}
+}else{
+    define('ENVIRONMENT','production' );
+}
 /*
  *---------------------------------------------------------------
  * ERROR REPORTING
@@ -76,17 +76,18 @@ switch (ENVIRONMENT)
 
 	case 'testing':
 	case 'production':
+//    error_reporting(-1);
+//		ini_set('display_errors', 0);
+//		if (version_compare(PHP_VERSION, '5.3', '>='))
+//		{
+//			error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
+//		}
+//		else
+//		{
+//			error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
+//		}
     error_reporting(-1);
-		ini_set('display_errors', 0);
-		if (version_compare(PHP_VERSION, '5.3', '>='))
-		{
-			error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
-		}
-		else
-		{
-			error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
-		}
-
+    ini_set('display_errors', 1);
 	break;
 
 	default:
