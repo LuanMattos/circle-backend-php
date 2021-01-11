d39ae0f1a9a62c7783015f9fac11633d345bcf0d
luanmattos <patrickluan.matos@gmail.com>
2021-01-11 00:49:24 -0300

    stoooop

--- application/controllers/MailMarketing.php
+++ application/controllers/MailMarketing.php
@@ -12,11 +12,11 @@ class MailMarketing extends Home_Controller
 
     public function index(){
         $email = $this->Email_marketing_model->getWhere(['email_marketing_sent'=>'f'], "row");
-
-        $email = 'patrick.mattos@compasso.com.br';
-        if( $email ){
-            $this->sendMail( $email );
-        }
+//
+//        $email = 'patrick.mattos@compasso.com.br';
+//        if( $email ){
+//            $this->sendMail( $email );
+//        }
 
 
 
