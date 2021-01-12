f022ec50cd0c0f87f9b739a3e03e3e7e7880e02d
patrick_mattos <patrick_mattos@terceiros.sicredi.com.br>
2021-01-12 17:53:51 -0300

    pause mail

--- application/controllers/MailMarketing.php
+++ application/controllers/MailMarketing.php
@@ -15,14 +15,14 @@ class MailMarketing extends Home_Controller
 
     public function index()
     {
-        $email = $this->Email_marketing_model->getWhere(['email_marketing_sent' => 'f'], "array", 'email_marketing_date', "DESC", 15, NULL);
-
-        foreach ($email as $row) {
-            $this->emailService->sendEmail($row['email_marketing_email']);
-            $this->Email_marketing_model->save(
-                ['email_marketing_id' => $row['email_marketing_id'], 'email_marketing_sent' => 't']
-            );
-        }
+//        $email = $this->Email_marketing_model->getWhere(['email_marketing_sent' => 'f'], "array", 'email_marketing_date', "DESC", 15, NULL);
+//
+//        foreach ($email as $row) {
+//            $this->emailService->sendEmail($row['email_marketing_email']);
+//            $this->Email_marketing_model->save(
+//                ['email_marketing_id' => $row['email_marketing_id'], 'email_marketing_sent' => 't']
+//            );
+//        }
 
     }
 }
