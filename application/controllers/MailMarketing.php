161445a4d6eff1d5b573094b7bb1a1ee86c5a3f5
patrick_mattos <patrick_mattos@terceiros.sicredi.com.br>
2021-01-12 14:53:02 -0300

    save

--- application/controllers/MailMarketing.php
+++ application/controllers/MailMarketing.php
@@ -1,29 +1,27 @@
 <?php
+
 use Services\Domain\User\EmailService;
+
 class MailMarketing extends Home_Controller
 {
     private $emailService;
 
-    public function __construct(){
+    public function __construct()
+    {
         parent::__construct();
         $this->load->model('mail/Email_marketing_model');
         $this->emailService = new EmailService\EmailService();
     }
 
-    public function index(){
-        $email = $this->Email_marketing_model->getWhere(['email_marketing_sent'=>'f'], "array", 'email_marketing_date', "DESC", 15, NULL);
+    public function index()
+    {
+        $email = $this->Email_marketing_model->getWhere(['email_marketing_sent' => 'f'], "array", 'email_marketing_date', "DESC", 15, NULL);
 
-        if(count($email)){
-            foreach($email as $row){
-                if (isset($row['email_marketing_email'])) {
-
-                    $this->emailService->sendEmail($row['email_marketing_email']);
-                    $this->Email_marketing_model->save(
-                        ['email_marketing_id'=>$row['email_marketing_id'],'email_marketing_sent'=>'t']
-                    );
-                    $this->response($row['email_marketing_mail']);
-                }
-            }
+        foreach ($email as $row) {
+            $this->emailService->sendEmail($row['email_marketing_email']);
+            $this->Email_marketing_model->save(
+                ['email_marketing_id' => $row['email_marketing_id'], 'email_marketing_sent' => 't']
+            );
         }
 
     }
