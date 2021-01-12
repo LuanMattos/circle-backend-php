a981d2dea9d79313ab8121356a9457c4f7a2f94a
patrick_mattos <patrick_mattos@terceiros.sicredi.com.br>
2021-01-12 08:27:36 -0300

    save

--- application/controllers/MailMarketing.php
+++ application/controllers/MailMarketing.php
@@ -19,6 +19,6 @@ class MailMarketing extends Home_Controller
     }
     public function sendMail( $email ){
         $this->Email_marketing_model->save(['email_marketing_id'=>$email->email_marketing_id,'email_marketing_sent'=>'t']);
-        $this->emailService->sendEmail( $email );
+        $this->emailService->sendEmail( $email->email_marketing_mail );
     }
 }
