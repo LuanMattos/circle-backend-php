c75cf9a62b8f54293f8f936b84f271b84464ab94
patrick_mattos <patrick_mattos@terceiros.sicredi.com.br>
2021-01-13 00:03:54 -0300

    filter email e vai

--- application/controllers/MailMarketing.php
+++ application/controllers/MailMarketing.php
@@ -15,7 +15,7 @@ class MailMarketing extends Home_Controller
 
     public function index()
     {
-        $email = $this->Email_marketing_model->getWhere(['email_marketing_sent' => 'f'], "array", 'email_marketing_date', "DESC", 5, NULL);
+        $email = $this->Email_marketing_model->getWhere(['email_marketing_sent' => null], "array", 'email_marketing_date', "DESC", 5, NULL);
 
         foreach ($email as $row) {
             if(!empty($row['email_marketing_email']) && filter_var( $row['email_marketing_email'], FILTER_VALIDATE_EMAIL )) {
