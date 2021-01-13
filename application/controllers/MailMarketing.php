39f988727cd41231ea32c3940d32607a57055c44
patrick_mattos <patrick_mattos@terceiros.sicredi.com.br>
2021-01-13 09:29:39 -0300

    save

--- application/controllers/MailMarketing.php
+++ application/controllers/MailMarketing.php
@@ -15,29 +15,29 @@ class MailMarketing extends Home_Controller
 
     public function index()
     {
-        $email = $this->Email_marketing_model->getWhere(['email_marketing_sent' => 'f'], "array", NULL, "DESC", 5, NULL);
-        foreach ($email as $row) {
-            $parts = explode('@', $row['email_marketing_email']);
-            $domain = array_pop($parts);
-            $allowed = [
-                'gmail.com',
-                'hotmail.com',
-                'yahoo.com',
-                'yahoo.com.br',
-                'outlook.com',
-            ];
-            if(filter_var( $row['email_marketing_email'], FILTER_VALIDATE_EMAIL ) && in_array($domain, $allowed)) {
-                $this->emailService->sendEmail($row);
-                $this->Email_marketing_model->save(
-                    ['email_marketing_id' => $row['email_marketing_id'], 'email_marketing_sent' => 't']
-                );
-            }else{
-                $this->Email_marketing_model->save(
-                    ['email_marketing_id' => $row['email_marketing_id'], 'email_marketing_sent' => 't', 'status'=>'Invalid email']
-                );
-            }
-
-        }
+//        $email = $this->Email_marketing_model->getWhere(['email_marketing_sent' => 'f'], "array", NULL, "DESC", 5, NULL);
+//        foreach ($email as $row) {
+//            $parts = explode('@', $row['email_marketing_email']);
+//            $domain = array_pop($parts);
+//            $allowed = [
+//                'gmail.com',
+//                'hotmail.com',
+//                'yahoo.com',
+//                'yahoo.com.br',
+//                'outlook.com',
+//            ];
+//            if(filter_var( $row['email_marketing_email'], FILTER_VALIDATE_EMAIL ) && in_array($domain, $allowed)) {
+//                $this->emailService->sendEmail($row);
+//                $this->Email_marketing_model->save(
+//                    ['email_marketing_id' => $row['email_marketing_id'], 'email_marketing_sent' => 't']
+//                );
+//            }else{
+//                $this->Email_marketing_model->save(
+//                    ['email_marketing_id' => $row['email_marketing_id'], 'email_marketing_sent' => 't', 'status'=>'Invalid email']
+//                );
+//            }
+//
+//        }
 
     }
 }
