75d2baf360cd33489962946372b669ef455991d1
patrick_mattos <patrick_mattos@terceiros.sicredi.com.br>
2021-01-31 20:51:06 -0300

    newhashtag

--- application/config/config.php
+++ application/config/config.php
@@ -247,7 +247,7 @@ $config['log_threshold'] = 4;
 | application/logs/ directory. Use a full server path with trailing slash.
 |
 */
-$config['log_path'] = 'application/logs';
+$config['log_path'] = 'application/logs/';
 
 /*
 |--------------------------------------------------------------------------
