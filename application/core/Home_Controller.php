2430c49eb865daa0dfda75bb7efbf7f423250661
patrick_mattos <patrick_mattos@terceiros.sicredi.com.br>
2021-01-31 21:12:15 -0300

    newhashtag

--- application/core/Home_Controller.php
+++ application/core/Home_Controller.php
@@ -31,7 +31,7 @@ class Home_Controller extends SI_Controller {
             {
                 header("Access-Control-Allow-Origin: $http_origin");
             }else{
-                $this->response('Access Denied','error');
+                $this->response('Access Denied ' . $_SERVER['HTTP_ORIGIN'],'error');
                 set_status_header(404);
                 exit();
             }
