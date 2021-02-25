5003e91eda4f54aaf8f6705936c603112cef439a
patrick_mattos <patrick_mattos@terceiros.sicredi.com.br>
2021-02-24 21:42:52 -0300

    task gambi front

--- application/controllers/Task.php
+++ application/controllers/Task.php
@@ -9,7 +9,7 @@ use Services\Domain\Monetization\MonetizationService;
 use Services\Domain\User\EmailService;
 
 
-class Monetization extends Home_Controller
+class Task extends Home_Controller
 {
     private $jwt;
     private $http;
@@ -31,27 +31,22 @@ class Monetization extends Home_Controller
         $this->s3 = new StorageService\StorageService();
         $this->monetizationService = new MonetizationService\MonetizationService();
         $this->monetizationRepository = new MoneyRepository\MonetizationRepository();
+
+        /** 1 min **/
+        $this->sendEmailInvite();
+        $this->sendEmailLembreteZero();
     }
 
-    public function saveCodeConfirmationMoney(){
-        $dataJwt   = $this->jwt->decode();
-        $data = (object)$this->http::getDataHeader();
-        if(isset($data->userName)) {
-            $this->monetizationRepository->saveMonetizationConfirmUser($dataJwt->user_id, $data->userName);
-        }
+    /**
+     * @Cron
+    **/
+    public function sendEmailInvite(){
+            $this->emailService->sendEmailInviteLine();
     }
-    public function saveEmailInvite(){
-        $dataJwt   = $this->jwt->decode();
-        $data = (object)$this->http::getDataHeader();
-        if( isset($data->data->fullName) && isset($data->data->email) ) {
-            $full_name = $data->data->fullName;
-            $email = $data->data->email;
-            $this->monetizationService->saveEmailInvite($full_name, $email, $dataJwt);
-        }
+    /**
+     * @Cron
+    **/
+    public function sendEmailLembreteZero(){
+            $this->emailService->sendLembreteZero();
     }
-    public function getDataDashBoard(){
-        $dataJwt   = (object)$this->jwt->decode();
-        $this->monetizationRepository->getDataDashboard( $dataJwt );
-    }
-
 }
