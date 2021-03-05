b614f3d88726a74bc0185ad058ffe776ea53a624
Patrick Luan de Mattos <44415640+LuanMattos@users.noreply.github.com>
2021-03-05 12:18:10 -0300

    Update StorageService.php

--- application/services/domain/storage/StorageService.php
+++ application/services/domain/storage/StorageService.php
@@ -10,7 +10,10 @@ class StorageService extends GeneralService{
 
     private $s3;
     private $s3Config;
+    private $s3ConfigMovie;
     private $endpoint;
+    private $endpointVideo;
+    private $endpointVideoUrl;
     private $userRepository;
     private $photoRepository;
 
@@ -18,7 +21,10 @@ class StorageService extends GeneralService{
         parent::__construct();
         $this->s3 = new Amazon\S3();
         $this->s3Config = $this->config->item('bucket_name');
+        $this->s3ConfigMovie = $this->config->item('bucket_name_video');
         $this->endpoint = $this->config->item('end_point');
+        $this->endpointVideo = $this->config->item('end_point_video');
+        $this->endpointVideoUrl = $this->config->item('end_point_video_url');
         $this->userRepository = new User\UserRepository();
         $this->photoRepository = new Photo\PhotoRepository();
     }
@@ -29,14 +35,15 @@ class StorageService extends GeneralService{
         $fileName = md5($file['name'] . time());
 
         try{
-            $this->s3->putObjectFile( $file['tmp_name'], $bucketName, $name_folder_user . '/' . $fileName, Amazon\S3::ACL_PUBLIC_READ );
-            $url = 'https://'. $this->endpoint . '/' . $bucketName . '/' . $name_folder_user . '/' .  $fileName;
-            $dataPhotoRepository = [
-                'user_name' => $user->user_name,
-                'name_folder' => $name_folder_user
-            ];
-            $this->userRepository->updateUserByUserName( $dataPhotoRepository );
-            $this->photoRepository->saveImage( $datapost, $user, $url, $type );
+            if($this->s3->putObjectFile( $file['tmp_name'], $bucketName, $name_folder_user . '/' . $fileName, Amazon\S3::ACL_PUBLIC_READ )) {
+                $url = 'https://'. $this->endpoint . '/' . $bucketName . '/' . $name_folder_user . '/' .  $fileName;
+                $dataPhotoRepository = [
+                    'user_name' => $user->user_name,
+                    'name_folder' => $name_folder_user
+                ];
+                $this->userRepository->updateUserByUserName($dataPhotoRepository);
+                $this->photoRepository->saveImageOrVideo($datapost, $user, $url, $type);
+            }
 
         }catch (\S3Exception $e ){
 
@@ -46,15 +53,50 @@ class StorageService extends GeneralService{
         self::Success( $result );
     }
 
-    public function removeImage( $uri ){
-        $bucketName = $this->s3Config;
+    public function saveMovie( $user, $file, $datapost ){
+        $bucketName = $this->s3ConfigMovie;
+        $name_folder_user = md5($user->user_name . $user->user_id);
+        $fileName = md5($file['name'] . time());
+
         try{
-            $lenUrlCrop = strlen('https://' . $this->endpoint . '/' . $bucketName . '/');
-            $urlLen = strlen( $uri );
-            $uriFormatted = substr( $uri, $lenUrlCrop, $urlLen );
-            $result = $this->s3->deleteObject( $bucketName, $uriFormatted );
+            if($this->s3->putObjectFile( $file['tmp_name'], $bucketName, $name_folder_user . '/' . $fileName, Amazon\S3::ACL_PUBLIC_READ )) {
+                $url = 'https://' . $bucketName . "." .  $this->endpointVideoUrl . '/' . $name_folder_user . '/' . $fileName;
+                $dataPhotoRepository = [
+                    'user_name' => $user->user_name,
+                    'name_folder' => $name_folder_user
+                ];
+                $this->userRepository->updateUserByUserName($dataPhotoRepository);
+                $this->photoRepository->saveImageOrVideo($datapost, $user, $url, 'video');
+            }
         }catch (\S3Exception $e ){
+
             $result = $e->getMessage();
+            debug($result);
+        }
+        self::Success( $result );
+    }
+
+    public function removeImage( $uri ){
+        if (strpos($uri,'/circle-photo/')){
+            $bucketName = $this->s3Config;
+            try{
+                $lenUrlCrop = strlen('https://' . $this->endpoint . '/' . $bucketName . '/');
+                $urlLen = strlen( $uri );
+                $uriFormatted = substr( $uri, $lenUrlCrop, $urlLen );
+                $result = $this->s3->deleteObject( $bucketName, $uriFormatted );
+            }catch (\S3Exception $e ){
+                $result = $e->getMessage();
+            }
+        }else{
+            $bucketName = $this->s3ConfigMovie;
+            try{
+                $lenUrlCrop = strlen('https://' . $bucketName . "." .  $this->endpointVideoUrl . "/");
+                $urlLen = strlen( $uri );
+                $uriFormatted = substr( $uri, $lenUrlCrop, $urlLen );
+                $result = $this->s3->deleteObject( $bucketName, $uriFormatted );
+            }catch (\S3Exception $e ){
+                $result = $e->getMessage();
+            }
         }
         self::Success( $result );
     }
