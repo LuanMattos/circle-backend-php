acabf354061562f074165797330476693681e468
patrick_mattos <patrick_mattos@terceiros.sicredi.com.br>
2021-07-05 23:14:35 -0300

    mining

--- application/core/Home_Controller.php
+++ application/core/Home_Controller.php
@@ -28,7 +28,7 @@ class Home_Controller extends SI_Controller {
             $http_origin = $_SERVER['HTTP_ORIGIN'];
 
             if (
-                ($http_origin == $this->elb_ip[0] || $http_origin == "https://mycircle.click" || $http_origin == "127.0.0.1" || $_SERVER['HTTP_HOST'] == "be.mycircle.click")
+                ($http_origin == $this->elb_ip[0] || $http_origin == "https://mycircle.click")
             )
             {
                 header("Access-Control-Allow-Origin: $http_origin");
