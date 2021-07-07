80d1441d86d7bc1be2cc677c544b54b62b04cc48
patrick_mattos <patrick_mattos@terceiros.sicredi.com.br>
2021-07-07 10:27:58 -0300

    ajuste prod

--- application/controllers/MiningTwitter.php
+++ application/controllers/MiningTwitter.php
@@ -41,7 +41,7 @@ class MiningTwitter extends Home_Controller
 
     public function saveUserById()
     {
-        $hashtag = 'photography';
+        $hashtag = 'photography, ohio, oregon, nature, travel, vsco';
         $url = 'https://api.twitter.com/1.1/search/tweets.json';
 
 
