efd7fadc863fbe81e4940cb72ba1a52ec797e15c
patrick_mattos <patrick_mattos@terceiros.sicredi.com.br>
2021-07-08 22:44:53 -0300

    sve

--- application/core/Home_Controller.php
+++ application/core/Home_Controller.php
@@ -26,7 +26,7 @@ class Home_Controller extends SI_Controller {
             header('Access-Control-Allow-Origin: *');
         }else{
             $http_origin = $_SERVER['HTTP_ORIGIN'];
-            echo $http_origin;
+            debug($http_origin);
 
             if (
                 ($http_origin == $this->elb_ip[0] || $http_origin == "https://mycircle.click" || $http_origin == "127.0.0.1")
